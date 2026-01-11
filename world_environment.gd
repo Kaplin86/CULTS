b4612448eeac49ba00b387bd83a81257a645a6b8
Kaplin86 <87865073+Kaplin86@users.noreply.github.com>
2026-01-11 15:05:12 -0500

    metaball
    lowkey i think the inspo for this uses a different method so i might redo

--- world_environment.gd
+++ world_environment.gd
@@ -4,6 +4,7 @@ var ramp : Gradient = shader.get_shader_parameter("tex").color_ramp
 
 var dt = 0.0
 func _process(delta):
+	return
 	dt += delta * 0.5
 	print(ramp.offsets)
 	ramp.set_offset(1,sin(dt) * 0.4 + 0.5)
