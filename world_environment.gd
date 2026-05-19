d196318952588c7e7aa0744a2f5a591219e39211
Kaplin86 <87865073+Kaplin86@users.noreply.github.com>
2026-05-19 19:55:09 -0400

    player color only effects after turn. fixed sky lighting thing

--- world_environment.gd
+++ world_environment.gd
@@ -16,20 +16,22 @@ func _process(delta):
 	
 	var suspectedColor = Color.WHITE
 	if References.boardHandler.currentPlayer:
-		for I in References.boardHandler.currentPlayer.pool:
-			for E in References.boardHandler.currentPlayer.pool[I]:
-				suspectedColor = lerp(suspectedColor,References.typeToColor[I],0.5)
+		suspectedColor = getBoardColor(References.boardHandler.currentPlayer)
 	
 	ramp.set_color(1,ramp.get_color(1).lerp(suspectedColor,delta * 1))
 	if !firstSet:
 		firstSet = true
 		ramp.set_color(1,suspectedColor)
-	var modelNum = References.boardHandler.playerObjects.find(References.boardHandler.currentPlayer)
-	if modelNum != -1:
-		var model = $"../PlayerSprites".get_child(modelNum)
-		print("model is ", model.name)
-		print("children are", model.get_children()," and child 0 is", model.get_child(0))
-		var mat = model.find_child("Node3D2").mat
-		mat.albedo_color = lerp(mat.albedo_color,suspectedColor,0.5)
-		print(modelNum)
+	for I in References.boardHandler.playerObjects.size():
+		if References.boardHandler.playerObjects[I] != References.boardHandler.currentPlayer:
+			var model = $"../PlayerSprites".get_child(I)
+			var mat = model.find_child("Node3D2").mat
+			mat.albedo_color = lerp(mat.albedo_color,getBoardColor(References.boardHandler.playerObjects[I]),0.1)
 	
+
+func getBoardColor(player : PlayerResource):
+	var suspectedColor = Color.WHITE
+	for I in player.pool:
+		for E in player.pool[I]:
+			suspectedColor = lerp(suspectedColor,References.typeToColor[I],0.5)
+	return suspectedColor
